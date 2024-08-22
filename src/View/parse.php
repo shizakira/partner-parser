cb9bb594c7adffb4934e622d12b6d6900123be4e
shizakira <ikirachn@gmail.com>
2024-08-22 18:46:53 +0500

    fix css

--- src/View/parse.php
+++ src/View/parse.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Парсер</title>
+    <link rel="stylesheet" href="../View/style.css">
 </head>
 
 <body>
