c6ef2f26f07caae95b5a88d4a035c9e4da3463ed
shizakira <ikirachn@gmail.com>
2024-08-22 18:53:34 +0500

    dop fix css

--- src/Classes/Paginator.php
+++ src/Classes/Paginator.php
@@ -31,8 +31,8 @@ class Paginator extends Database
             [$id, $name, $detailUrl, $address] = array_values($el);
             $html = <<<HTML
                 <div class="partner-row">
-                    <p>ID записи: $id</p>
-                    <p>Партнер: <a href="$detailUrl" target="_blank">$name</a></p>
+                    <p><span class='bold'>ID записи:</span> $id</p>
+                    <p><span class='bold'>Партнер:</span> <a href="$detailUrl" target="_blank">$name</a></p>
                     <p><a href="$address" target="_blank">Адрес сайта партнера</a></p>
                     <p><a href ="$detailUrl" target="_blank">Детальная страница на битриксе</a></p>
                     <p><a href="/?projects=true&id=$id" target="_blank">Проекты партнера</a></p>
@@ -53,10 +53,10 @@ class Paginator extends Database
             [$id,, $projectUrl, $productVersion, $description] = array_values($el);
             $html = <<<HTML
                 <div class="project-row">
-                    <p>ID проекта: $id</p>
-                    <p>Адрес проекта: <a href="$projectUrl" target="_blank">$projectUrl</a></p>
-                    <p>Версия продукта: $productVersion</p>
-                    <p>Описание проекта: $description</p>
+                    <p><span class='bold'>ID проекта:</span> $id</p>
+                    <p><span class='bold'>Адрес проекта:</span> <a href="$projectUrl" target="_blank">$projectUrl</a></p>
+                    <p><span class='bold'>Версия продукта:</span> $productVersion</p>
+                    <p><span class='bold'>Описание проекта:</span> $description</span></p>
                 </div>
             HTML;
 
